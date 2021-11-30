217aa331527dc537ac08f34d8ad25d3cd9a662be
Antony <56368902+Dartosh@users.noreply.github.com>
2021-11-30 23:37:56 +0300

    init: upload project files

---
+++ lab-3/cmake-build-debug/CMakeFiles/Lab_3_1.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Sem5/OC/Lab 3.1/adress_spaces.c" "D:/Sem5/OC/Lab 3.1/cmake-build-debug/CMakeFiles/Lab_3_1.dir/adress_spaces.c.obj"
+  "D:/Sem5/OC/Lab 3.1/load_test.c" "D:/Sem5/OC/Lab 3.1/cmake-build-debug/CMakeFiles/Lab_3_1.dir/load_test.c.obj"
+  "D:/Sem5/OC/Lab 3.1/main.c" "D:/Sem5/OC/Lab 3.1/cmake-build-debug/CMakeFiles/Lab_3_1.dir/main.c.obj"
+  "D:/Sem5/OC/Lab 3.1/mmemory.c" "D:/Sem5/OC/Lab 3.1/cmake-build-debug/CMakeFiles/Lab_3_1.dir/mmemory.c.obj"
+  "D:/Sem5/OC/Lab 3.1/segment_table.c" "D:/Sem5/OC/Lab 3.1/cmake-build-debug/CMakeFiles/Lab_3_1.dir/segment_table.c.obj"
+  "D:/Sem5/OC/Lab 3.1/unit_test.c" "D:/Sem5/OC/Lab 3.1/cmake-build-debug/CMakeFiles/Lab_3_1.dir/unit_test.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab-3/cmake-build-debug/CMakeFiles/Lab_3_1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_3_1.dir/adress_spaces.c.obj"
+  "CMakeFiles/Lab_3_1.dir/load_test.c.obj"
+  "CMakeFiles/Lab_3_1.dir/main.c.obj"
+  "CMakeFiles/Lab_3_1.dir/mmemory.c.obj"
+  "CMakeFiles/Lab_3_1.dir/segment_table.c.obj"
+  "CMakeFiles/Lab_3_1.dir/unit_test.c.obj"
+  "Lab_3_1.exe"
+  "Lab_3_1.exe.manifest"
+  "Lab_3_1.pdb"
+  "libLab_3_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_3_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
